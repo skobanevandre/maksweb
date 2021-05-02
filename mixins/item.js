44d9fb0543ea822dfafcad12f36fb1b7f91f487b
skobanevandre <skobanev@yandex.ru>
2021-05-02 10:45:26 +0300

    sort

--- mixins/item.js
+++ mixins/item.js
@@ -57,6 +57,58 @@ export default {
     */
     fixPrice( price ) {
       return ( ( ( parseFloat( ( price / 10 ).toFixed( ) ) + 1 ) * 10 ) - 1 ).toFixed( 2 );
-    }  
+    },
+    
+    /**
+     * 
+     * @param {*} field 
+     * @param { NUMBER } order 
+     * @returns 
+     */
+    compare( field, order ) {
+      var len = arguments.length;
+      
+      if(len === 0) { 
+        return ( a, b ) => ( a < b && -1 ) || ( a > b && 1 ) || 0; 
+      }
+      
+      if(len === 1) {
+        switch( typeof field ) {
+          case 'number':
+            return field < 0 ? 
+              ( ( a, b ) => ( a < b && 1 ) || ( a > b && -1 ) || 0 ) :
+              ( ( a, b ) => ( a < b && -1 ) || ( a > b && 1 ) || 0 );
+            case 'string':
+              return ( a, b ) => ( a[ field ] < b[ field ] && -1 ) || ( a[ field ] > b[ field ] && 1 ) || 0;
+          } 
+      }
+      
+      if( len === 2 && typeof order === 'number' ) {
+        return order < 0 ?
+          ( ( a, b ) => ( a[ field ] < b[ field ] && 1 ) || ( a[ field ] > b[ field ] && -1 ) || 0 ) :
+          ( ( a, b ) => ( a[ field ] < b[ field ] && -1 ) || ( a[ field ] > b[ field ] && 1 ) || 0 );
+      }
+      var fields, orders;
+      if( typeof field === 'object' ) {
+        fields = Object.getOwnPropertyNames( field );
+        orders = fields.map( key => field[ key ] );
+        len = fields.length;
+      } else {
+        fields = new Array( len );
+        orders = new Array( len );
+        for( let i = len; i--; ) {
+          fields[ i ] = arguments[ i ];
+          orders[ i ] = 1;
+        }
+      }
+      return ( a, b ) => {
+        for( let i = 0; i < len; i++ ) {
+          if( a[ fields[ i ] ] < b[ fields[ i ] ] ) return orders[ i ];
+          if( a[ fields[ i ] ] > b[ fields[ i ] ] ) return -orders[ i ];
+        }
+        return 0;
+      };
+    },
+
   }
 }
--- pages/category/_category.vue
+++ pages/category/_category.vue
@@ -1,18 +1,70 @@
 <template>
   <div class="margin-top">
     <titletext class="center">{{ category.name }}</titletext>
+    
+    <vs-row justify="center" align="center" class="margin-top">
+      <span class="margin-right">Сортировать по: </span>
+      <vs-select v-model="sort" class="sort" @change="doSort()">
+        <vs-option label="Рекомендуемые" value="0">Рекомендуемые</vs-option>
+        <vs-option label="Возрастанию цены" value="1">Возрастанию цены</vs-option>
+        <vs-option label="Убыванию цены" value="2">Убыванию цены</vs-option>
+      </vs-select>
+    </vs-row>
+    
     <div class="cards margin-top">
       <card 
-        v-for="( item, j) in items"
+        v-for="( item, j ) in ritems"
         :key="j"
         :item="item" />
     </div>
 
+  <pre> {{ items }} </pre>
   </div>  
 </template>
 
 <script>
+
 export default {
+
+  data() {
+    return {
+      sort: '0',
+      ritems: [],
+    }
+  },
+
+  methods: {
+
+    doSort( ) {
+      switch( this.sort ) {
+        case '0':
+          this.ritems.splice( 0, this.ritems.length );
+          this.$nextTick( () => {
+            this.ritems = Object.assign( [], this.items.sort( ( a, b ) => ( Number( a.sold ) < Number( b.sold ) && 1 ) || ( Number( a.sold ) > Number( b.sold ) && -1 ) || 0 ) ); 
+          } );  
+        break;
+
+        case '1': 
+          this.ritems.splice( 0, this.ritems.length );
+          this.$nextTick( () => {
+            this.ritems = Object.assign( [], this.items.sort( ( a, b ) => ( Number( a.price.standart ) > Number( b.price.standart ) && 1 ) || ( Number( a.price.standart ) < Number( b.price.standart ) && -1 ) || 0 ) ); 
+          } );  
+        break;
+
+        case '2': 
+          this.ritems.splice( 0, this.ritems.length );
+          this.$nextTick( () => {
+            this.ritems = Object.assign( [], this.items.sort( ( a, b ) => ( Number( a.price.standart ) < Number( b.price.standart ) && 1 ) || ( Number( a.price.standart ) > Number( b.price.standart ) && -1 ) || 0 ) ); 
+          } )  
+        break;
+      }
+    },
+
+  },
+
+  mounted( ) {
+    this.doSort();
+  },
 
   async asyncData({ params, $axios }) {
     const result = await $axios.$get( '/categoryitems/' + params.category );
@@ -22,10 +74,14 @@ export default {
 </script>
 
 <style scoped>
-.cards {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: flex-start;
-}
+  .cards {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+  }
+
+  .sort {
+    width: 200px;  
+  }
 </style>
 
